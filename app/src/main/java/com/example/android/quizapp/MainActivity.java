b31363e42eb379a991af3078b370bc1c2c52da73
jellydragon <mary.romanenko@gmail.com>
2017-05-20 11:53:17 +0200

    a few corrections in logic and layout

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.android.quizapp;
 
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
@@ -12,9 +12,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String currentState = "q0"; // intro
+    private int nCorrect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +25,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private String currentState = "q0"; // intro
-    private int nCorrect = 0;
-
-
     public void NextQuestion(View V) {
 
         switch (currentState) {
             case "q0":
                 // show next question
-                ((TextView) findViewById(R.id.question_view)).setText(R.string.q1);
+                ((TextView) findViewById(R.id.question_view))
+                        .setText(R.string.q1);
 
                 // show new image
-                Drawable id = ResourcesCompat.getDrawable(getResources(), R.drawable.baeckeoffe, null);
+                Drawable id = ResourcesCompat.getDrawable(getResources(),
+                        R.drawable.baeckeoffe, null);
+
                 ((ImageView) findViewById(R.id.img_view)).setImageDrawable(id);
 
                 // show next answer view
@@ -46,12 +48,13 @@ public class MainActivity extends AppCompatActivity {
                 currentState = "q1";
                 break;
 
-            case "q1" :
+            case "q1":
                 // check answer
                 if (((RadioButton) findViewById(R.id.q1_a3)).isChecked()) nCorrect++;
 
                 // show next question
-                ((TextView) findViewById(R.id.question_view)).setText(R.string.q2);
+                ((TextView) findViewById(R.id.question_view))
+                        .setText(R.string.q2);
 
                 // hide prev answer view
                 findViewById(R.id.q1_answers).setVisibility(View.GONE);
@@ -66,15 +69,15 @@ public class MainActivity extends AppCompatActivity {
                 currentState = "q2";
                 break;
 
-
-            case "q2" :
+            case "q2":
                 // check answer
-                if (((EditText) findViewById(R.id.user_input)).getText().toString().equals(getString(R.string.q2_a1))) {
+                if (((EditText) findViewById(R.id.user_input)).getText()
+                        .toString().equalsIgnoreCase(getString(R.string.q2_a1)))
                     nCorrect++;
-                }
 
                 // print next question
-                ((TextView) findViewById(R.id.question_view)).setText(R.string.q3);
+                ((TextView) findViewById(R.id.question_view))
+                        .setText(R.string.q3);
 
                 // hide prev answer view
                 findViewById(R.id.user_input).setVisibility(View.GONE);
@@ -90,16 +93,20 @@ public class MainActivity extends AppCompatActivity {
 
                 break;
 
-            case "q3" :
+            case "q3":
                 // check answer
-                boolean correct1 = ((CheckBox) findViewById(R.id.q3_a1)).isChecked();
-                boolean correct2 = ((CheckBox) findViewById(R.id.q3_a3)).isChecked();
-                boolean correct3 = ((CheckBox) findViewById(R.id.q3_a4)).isChecked();
+                boolean q3Correct1 = ((CheckBox) findViewById(R.id.q3_a1)).isChecked();
+                boolean q3Correct2 = ((CheckBox) findViewById(R.id.q3_a3)).isChecked();
+                boolean q3Correct3 = ((CheckBox) findViewById(R.id.q3_a4)).isChecked();
+                boolean q3Incorrect1 = ((CheckBox) findViewById(R.id.q3_a2)).isChecked();
+                boolean q3Incorrect2 = ((CheckBox) findViewById(R.id.q3_a5)).isChecked();
 
-                if (correct1 & correct2 & correct3)  nCorrect++;
+                if (q3Correct1 && q3Correct2 && q3Correct3 && !q3Incorrect1 && !q3Incorrect2)
+                    nCorrect++;
 
                 // print next question
-                ((TextView) findViewById(R.id.question_view)).setText(R.string.q4);
+                ((TextView) findViewById(R.id.question_view))
+                        .setText(R.string.q4);
 
                 // show new image
                 id = ResourcesCompat.getDrawable(getResources(), R.drawable.spaetzle, null);
@@ -111,18 +118,17 @@ public class MainActivity extends AppCompatActivity {
                 // show next answer view
                 findViewById(R.id.q4_answers).setVisibility(View.VISIBLE);
 
-
                 currentState = "q4";
 
                 break;
 
-            case "q4" :
-
+            case "q4":
                 // check answer
                 if (((RadioButton) findViewById(R.id.q4_a1)).isChecked()) nCorrect++;
 
                 // print next question
-                ((TextView) findViewById(R.id.question_view)).setText(R.string.q5);
+                ((TextView) findViewById(R.id.question_view))
+                        .setText(R.string.q5);
 
                 // show new image
                 id = ResourcesCompat.getDrawable(getResources(), R.drawable.riesling, null);
@@ -138,16 +144,18 @@ public class MainActivity extends AppCompatActivity {
 
                 break;
 
-            case "q5" :
+            case "q5":
                 // check answer
-                correct1 = ((CheckBox) findViewById(R.id.q5_a1)).isChecked();
-                correct2 = ((CheckBox) findViewById(R.id.q5_a2)).isChecked();
-                correct3 = ((CheckBox) findViewById(R.id.q5_a3)).isChecked();
+                boolean q5Correct1 = ((CheckBox) findViewById(R.id.q5_a1)).isChecked();
+                boolean q5Correct2 = ((CheckBox) findViewById(R.id.q5_a2)).isChecked();
+                boolean q5Correct3 = ((CheckBox) findViewById(R.id.q5_a3)).isChecked();
+                boolean q5Incorrect1 = ((CheckBox) findViewById(R.id.q5_a4)).isChecked();
 
-                if (correct1 & correct2 & correct3)  nCorrect++;
+                if (q5Correct1 && q5Correct2 && q5Correct3 && !q5Incorrect1) nCorrect++;
 
                 // print next question
-                ((TextView) findViewById(R.id.question_view)).setText(R.string.q6);
+                ((TextView) findViewById(R.id.question_view))
+                        .setText(R.string.q6);
 
                 // show new image
                 id = ResourcesCompat.getDrawable(getResources(), R.drawable.bredele, null);
@@ -163,12 +171,13 @@ public class MainActivity extends AppCompatActivity {
 
                 break;
 
-            case "q6" :
+            case "q6":
                 // check answer
                 if (((RadioButton) findViewById(R.id.q6_a3)).isChecked()) nCorrect++;
 
                 // print next question
-                ((TextView) findViewById(R.id.question_view)).setText(R.string.q7);
+                ((TextView) findViewById(R.id.question_view))
+                        .setText(R.string.q7);
 
                 // show new image
                 id = ResourcesCompat.getDrawable(getResources(), R.drawable.cuisses, null);
@@ -180,16 +189,19 @@ public class MainActivity extends AppCompatActivity {
                 // show new answer view
                 ((EditText) findViewById(R.id.user_input)).setText("");
                 ((EditText) findViewById(R.id.user_input)).setInputType(InputType.TYPE_CLASS_TEXT);
+                ((EditText) findViewById(R.id.user_input)).setHint(R.string.hint_enter_name);
+
                 findViewById(R.id.user_input).setVisibility(View.VISIBLE);
 
                 currentState = "q7";
 
                 break;
 
-            case "q7" :
+            case "q7":
                 // check answer
-                String answer_q7 = ((EditText) findViewById(R.id.user_input)).getText().toString().trim().toLowerCase();
-                if (answer_q7.equals(getString(R.string.q7_a1))) {
+                String answer_q7 = ((EditText) findViewById(R.id.user_input))
+                        .getText().toString().trim();
+                if (answer_q7.equalsIgnoreCase(getString(R.string.q7_a1))) {
                     nCorrect++;
                 }
 
@@ -200,11 +212,13 @@ public class MainActivity extends AppCompatActivity {
 
                 String result = getText(R.string.your_score) + " " + Integer.toString(score) + "% ! ";
 
+                // show toast with the score
+                Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+
                 switch (nCorrect) {
                     case 7:
                         result += getText(R.string.result1);
                         break;
-
                     case 6:
                         result += getText(R.string.result2);
                         break;
@@ -219,9 +233,14 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
 
-
                 // print result
                 ((TextView) findViewById(R.id.question_view)).setText(result);
+
+                if (score == 7) {
+                    Toast.makeText(this, R.string.well_played, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, R.string.try_again, Toast.LENGTH_SHORT).show();
+                }
 
                 // show new image
                 id = ResourcesCompat.getDrawable(getResources(), R.drawable.alsace, null);
@@ -234,17 +253,15 @@ public class MainActivity extends AppCompatActivity {
 
                 break;
 
-            case "end" :
-
+            case "end":
                 // reset counter
-
                 nCorrect = 0;
 
                 // reset answer fields
-
                 ((EditText) findViewById(R.id.user_input)).setText("");
-                ((EditText) findViewById(R.id.user_input)).setInputType(InputType.TYPE_CLASS_NUMBER);
-
+                ((EditText) findViewById(R.id.user_input))
+                        .setInputType(InputType.TYPE_CLASS_NUMBER);
+                ((EditText) findViewById(R.id.user_input)).setHint(R.string.hint_enter_number);
 
                 ((RadioButton) findViewById(R.id.q1_a1)).setChecked(false);
                 ((RadioButton) findViewById(R.id.q1_a2)).setChecked(false);
@@ -271,9 +288,9 @@ public class MainActivity extends AppCompatActivity {
                 ((CheckBox) findViewById(R.id.q5_a3)).setChecked(false);
                 ((CheckBox) findViewById(R.id.q5_a4)).setChecked(false);
 
-
                 // show intro
-                ((TextView) findViewById(R.id.question_view)).setText(R.string.welcome);
+                ((TextView) findViewById(R.id.question_view))
+                        .setText(R.string.welcome);
 
                 // show Next button
                 ((Button) findViewById(R.id.button_next)).setText(R.string.next);
@@ -282,19 +299,14 @@ public class MainActivity extends AppCompatActivity {
                 currentState = "q0";
                 break;
 
-
-            default: break;
-
-
+            default:
+                break;
         }
-
-
     }
 
     private int CalculateScore() {
         int nQuestions = 7;
-        return ( (int) (((double) nCorrect )/ nQuestions * 100)) ;
+        return ((int) (((double) nCorrect) / nQuestions * 100));
     }
-
 
 }
